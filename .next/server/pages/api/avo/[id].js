3d3913fd3c505ee1719eee80117b3a2ebda68e21
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2021-09-20 00:00:07 -0500

    api

---
+++ .next/server/pages/api/avo/[id].js
@@ -0,0 +1,35 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/api/avo/[id]";
+exports.ids = ["pages/api/avo/[id]"];
+exports.modules = {
+
+/***/ "./pages/api/avo/[id].js":
+/*!*******************************!*\
+  !*** ./pages/api/avo/[id].js ***!
+  \*******************************/
+/***/ (() => {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbXSwiZmlsZSI6Ii4vcGFnZXMvYXBpL2F2by9baWRdLmpzLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/api/avo/[id].js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/api/avo/[id].js"));
+module.exports = __webpack_exports__;
+
+})();
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from "../components/NavBar/NavBar";
 
 export default function Home() {
+    const [productList, setProductList] = useState([]);
+    useEffect(() => {
+        fetch("/api/avo").then(response => response.json()).then(({ data, length }) => {
+            setProductList(data);
+        })
+
+    }, []);
+  
     return (
         <div>
-            <h1>Hola Mundo</h1>
+
             <NavBar />
+     
+                {productList.map((product) => (
+                    <div>{product.name}</div>
+                ))}
+           
+
         </div>
     )
 }
